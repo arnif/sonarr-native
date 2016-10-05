7c14a142625a38fdf2e70eaae2ad1f88ef42631b
Arni Fannar <arnifa@gmail.com>
2016-10-05 20:17:27 +0000

    Fix #29 (#30)

--- app/components/Series/SerieDetails.js
+++ app/components/Series/SerieDetails.js
@@ -94,7 +94,7 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'transparent',
     flexDirection: 'row',
